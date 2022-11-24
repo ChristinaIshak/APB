9c440fa5b463d5aeed8c13a4522c8e82cb2b7bd1
DESKTOP-NC6059S\User <christina02andreas@gmail.com>
2022-11-24 15:02:27 +0700

    Aplikasi Antrian Puskesmas

---
+++ flutter_application_antrian/android/app/src/main/kotlin/com/example/flutter_application_antrian/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_application_antrian
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_application_antrian/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_application_antrian/wrapper.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Wrapper(),
+    );
+  }
+}
